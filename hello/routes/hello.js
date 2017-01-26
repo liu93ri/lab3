6454a7a1cf1491c37e0c432a401078d35e926548
liu93ri <463788672@qq.com>
2017-01-26 11:25:26 -0800

    finish

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,14 @@
  */
 
 exports.view = function(req, res){
+	var nameToshow = req.params.userName;
+	if (nameToshow == undefined) {
+		nameToshow = 'World';
+	}
+
+	console.log("name is " + nameToshow);
+
   res.render('index', {
-  	'name': '',
+  	'name': nameToshow,
   });
 };
